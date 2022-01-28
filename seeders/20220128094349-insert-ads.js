42cf094399f6e19fdcb8ee06d06627e7e0c794b8
brorp <ryan.feb28@gmail.com>
2022-01-28 17:22:07 +0700

    C:/Program Files/Git/hotfix seed ads data 20

--- seeders/20220128094349-insert-ads.js
+++ seeders/20220128094349-insert-ads.js
@@ -30,6 +30,118 @@ module.exports = {
       status: 'Active',
       updatedAt: new Date (),
       createdAt: new Date ()
+    },{
+      title: "ADS 5",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 6",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 7",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 8",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 9",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 10",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 11",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 12",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 13",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 14",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 15",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 16",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 17",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 18",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 19",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
+    },{
+      title: "ADS 20",
+      url: 'https://www.w3.org/Out-Of-Date/hypertext/TBL_Disclaimer.html',
+      imgAds: "https://ik.imagekit.io/fjaskqdnu0xp/A_D_S_YRz-DR9Og.png?ik-sdk-version=javascript-1.4.3&updatedAt=1643363007631",
+      status: 'Active',
+      updatedAt: new Date (),
+      createdAt: new Date ()
     }]
     await queryInterface.bulkInsert("Ads", data)
   },
