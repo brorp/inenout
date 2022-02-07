f269715a1859643a01a14069266d479741724681
ryan <ryan.feb28@gmail.com>
2022-02-07 14:46:35 +0700

    add all tag featured

--- seeders/20220127033311-insert-featured-articles.js
+++ seeders/20220127033311-insert-featured-articles.js
@@ -2,34 +2,19 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const data = [{
-      title: "Socialising Spotify in App Experience: A Product Case Study",
-      caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
-      img: "https://ik.imagekit.io/fjaskqdnu0xp/images__349__NLHmNGQdK.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1643254350585",
-      articleId: 1,
-      isHomepage: true,
-      status: 'Active',
-      updatedAt: new Date (),
-      createdAt: new Date ()
-    },{
-      title: "Socialising Spotify in App Experience: A Product Case Study",
-      caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
-      img: "https://ik.imagekit.io/fjaskqdnu0xp/images__349__NLHmNGQdK.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1643254350585",
-      articleId: 2,
-      isHomepage: null,
-      status: 'Active',
-      updatedAt: new Date (),
-      createdAt: new Date ()
-    },{
-      title: "Socialising Spotify in App Experience: A Product Case Study",
-      caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
-      img: "https://ik.imagekit.io/fjaskqdnu0xp/images__349__NLHmNGQdK.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1643254350585",
-      articleId: 3,
-      isHomepage: null,
-      status: 'Active',
-      updatedAt: new Date (),
-      createdAt: new Date ()
-    }]
+    const data = []
+    for(let i = 0; i <= 22; i++){
+      data.push({
+        title: "Socialising Spotify in App Experience: A Product Case Study",
+        caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+        img: "https://ik.imagekit.io/fjaskqdnu0xp/images__349__NLHmNGQdK.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1643254350585",
+        articleId: i,
+        isHomepage: null,
+        status: 'Active',
+        updatedAt: new Date (),
+        createdAt: new Date ()
+      })
+    }
     await queryInterface.bulkInsert("FeaturedArticles", data)
   },
 
