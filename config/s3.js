1ed8e0e4eac9ce0ddd8575cd5246bf0fcdd30d98
brorp <ryan.feb28@gmail.com>
2022-03-11 13:49:28 +0700

    delete seed

--- config/s3.js
+++ config/s3.js
@@ -1,20 +1,27 @@
-// const aws = requrie('aws-sdk') ;
-// import multer from 'multer';
-// import multerS3 from 'multer-s3';
+const aws = require('aws-sdk') ;
 
-// if (process.env.NODE_ENV != 'production') {
-//     require('dotenv').config();
-// }
+if (process.env.NODE_ENV != 'production') {
+    require('dotenv').config();
+}
+require('dotenv').config();
+aws.config.update({
+    secretAccessKey: process.env.S3_SECRETKEY,
+    accessKeyId: process.env.S3_ACCESSKEY,
+    region: process.env.S3_REGION
+});
 
-// aws.config.update({
-//     secretAccessKey: process.env.S3_SECRETKEY,
-//     accessKeyId: process.env.S3_ACCESSKEY,
-//     region: process.env.S3_REGION
-// });
+const s3 = new aws.S3({
+    Bucket: process.env.S3_BUCKETNAME,
+    signatureVersion: 'v4'
+});
 
-// const s3 = new aws.S3({
-//     Bucket: process.env.S3_BUCKETNAME,
-//     signatureVersion: 'v4'
-// });
+(async () => {
+    await s3.putObject({
+        Body: "asdasdasd",
+        Bucket: process.env.S3_BUCKETNAME,
+        Key: "test.txt"
+    })
+    .promise()
+})();
 
 // module.exports = s3;
