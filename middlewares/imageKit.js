e428be4858382c41f9414c86ba64615df1182e99
brorp <ryan.feb28@gmail.com>
2022-03-18 14:25:40 +0700

    fix imgk

--- middlewares/imageKit.js
+++ middlewares/imageKit.js
@@ -142,22 +142,23 @@ const randomName = getSalt((Date.now() + +Math.floor(Math.random() * 9999)).toSt
         console.log(result)
         let uploadedImage = result
         req.body.imgThumbnail = uploadedImage.url
-      }
 
-      else if(req.files.img){
-        let result = await imagekit.upload({
-          file: req.files.img[0].buffer.toString('base64'),
-          fileName: randomName,
-          folder: `/ARTICLES/${folderName}`,  
-        }).then(result => {
-          console.log(result + "<<<<<<<<<<<")
-          return result 
-        }).catch(error => {
-          next(error)
-        })
-        console.log(result)
-        let uploadedImage = await result
-        req.body.img = uploadedImage.url
+        if(req.files.img){
+          let result = await imagekit.upload({
+            file: req.files.img[0].buffer.toString('base64'),
+            fileName: randomName,
+            folder: `/ARTICLES/${folderName}`,  
+          }).then(result => {
+            console.log(result + "<<<<<<<<<<<")
+            return result 
+          }).catch(error => {
+            next(error)
+          })
+          console.log(result)
+          let uploadedImage = await result
+          req.body.img = uploadedImage.url
+        }
+        
       }
 
       else { 
